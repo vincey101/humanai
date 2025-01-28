afeacef797d423d175dc6e0ca8dcb5399bbf554c
deelaurence <laurie143pelumi@gmail.com>
2025-01-28 16:59:26 +0100

    feat: authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import { redirect } from 'next/navigation';
+import { useSelector } from 'react-redux';
+import { RootState } from './store';
+
 
 export default function Home() {
+  const { user } = useSelector((state: RootState) => state.user);
+  if(!user.user){
+    redirect('/login')
+  }
   return (
     <div className="max-w-4xl mx-auto p-8">
-      <h1 className="text-4xl font-bold text-gray-900 mb-6">Welcome to Interactive Avatar Creator</h1>
+
+      <h1 className="text-4xl font-bold text-gray-900 mb-6">Hi {user.user.name.split(' ')[0]}, Welcome to Interactive Avatar Creator</h1>
       <div className="bg-white rounded-lg shadow-sm p-8 border border-gray-200">
         <p className="text-lg text-gray-600 mb-4">
           Create and manage your interactive avatars using our powerful tools.
---
+++ src/services/media.service.ts
@@ -0,0 +1,6 @@
+class Media {
+    authBackgroundImage = 'https://images.unsplash.com/photo-1737644467636-6b0053476bb2?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+}
+
+
+export default new Media()
